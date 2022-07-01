5eb862603b9b806afc3c16e4ef29cd49034f6db6
desnetgirum <desbe2020@gmail.com>
2022-06-30 18:46:44 -0700

    second

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
  #include <stdio.h>
-/*
- *main -Entry point
- *Return:always 0 success
-
-#include <string.h>
 /**
  * main - entry point,
  *
  * Return:  0
  */
 int main(void)
-
 {	
 puts("\"Programming is like building a multilingual puzzle");	
 return (0);
